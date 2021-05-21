afbdacef0d5c3a067c79f72c3022ef484939c936
Christopher Blöcker <mail@chrisbloecker.se>
2021-05-21 16:26:16 +0200

    added type annotations and implemented probability-based scoring

--- src/pathcost/codebook.py
+++ src/pathcost/codebook.py
@@ -1,5 +1,5 @@
 from numpy  import log2, inf
-from typing import List
+from typing import List, Dict
 
 class CodeBook:
     """
@@ -9,15 +9,36 @@ class CodeBook:
     """
     def __init__(self) -> None:
         """Creates an empty codebook."""
-        self.code_book = dict()
+        self.code_book:  Dict[int, CodeBook] = dict()
+        self.flow:       float = 0.0
+        self.enter:      float = 0.0
+        self.exit:       float = 0.0
+        self.normaliser: float = 0.0
 
     def __repr__(self) -> str:
         """
+        Serialises the codebook.
+
+        Returns
+        -------
+        str
+            The serialised codebook.
         """
         return f"<CodeBook\n{self._serialise(indent = 0)}\n>"
 
     def _serialise(self, indent: int) -> str:
         """
+        Serialises the codebook in a somewhat readable format.
+
+        Parameters
+        ----------
+        indent: int
+            The indentation level
+        
+        Returns
+        -------
+        str
+            The serialised codebook
         """
         subs = "".join([f"\n{cb._serialise(indent + 4)}" for cb in self.code_book.values()])
         return indent * " " + f"flow={self.flow:.2f}, enter={self.enter:.2f}, exit={self.exit:.2f}, norm={self.normaliser:.2f}, enter_cost={self.enter_cost:.2f}, exit_cost={self.exit_cost:.2f} {subs}"
@@ -50,9 +71,9 @@ class CodeBook:
         # We have reached the end of the path and insert the flows into the
         # current codebook.
         if len(path) == 0:
-            self.flow  = flow
-            self.enter = enter
-            self.exit  = exit
+            self.flow:  float = flow
+            self.enter: float = enter
+            self.exit:  float = exit
 
         # We need to descend through the hierarchy of codebooks, clipping off
         # one piece of the path per step.
@@ -75,7 +96,7 @@ class CodeBook:
         """
         Calculate the enter and exit costs, in bits, for all codebooks.
         """
-        self.exit_cost  = -log2(self.exit / self.normaliser) if self.normaliser > 0.0 and self.exit > 0.0 else inf
+        self.exit_cost: float  = -log2(self.exit / self.normaliser) if self.normaliser > 0.0 and self.exit > 0.0 else inf
         self.enter_cost = 0.0
 
         for m in self.code_book:
@@ -97,7 +118,7 @@ class CodeBook:
 
     def get_walk_cost(self, source: List[int], target: List[int]) -> float:
         """
-        Returns the walk cost between two paths.
+        Returns the walk cost, in bits, between two paths.
         For that, we first determine and clip off the common prefix of both paths.
         Then, we sum up the costs for exiting from the source path to the common
         ancestor and the costs for visiting the target path.
@@ -118,12 +139,17 @@ class CodeBook:
 
     def get_path_cost_forward(self, path: List[int]) -> float:
         """
-        Cost for descending along a path.
+        The cost, in bits, for descending along a path.
 
         Parameters
         ----------
         path: List[int]
             A path that addresses a node.
+        
+        Returns
+        -------
+        float
+            The cost, in bits, for descending along the given path.
         """
         if len(path) == 1:
             return self.code_book[path[0]].enter_cost
@@ -133,15 +159,88 @@ class CodeBook:
 
     def get_path_cost_reverse(self, path: List[int]) -> float:
         """
-        Cost for ascending along a path without visiting the starting node.
+        The cost, in bits, for ascending along a path without visiting the 
+        starting node.
 
         Parameters
         ----------
         path: List[int]
             A path that addresses a node.
+
+        Returns
+        -------
+        float
+            The cost in bits for ascending along the given path.
         """
         if len(path) == 1:
             return 0.0
 
         return self.code_book[path[0]].exit_cost \
              + self.code_book[path[0]].get_path_cost_reverse(path[1:])
+
+    def get_walk_probability(self, source: List[int], target: List[int]) -> float:
+        """
+        Probability of walking along the path from `source` to `target`.
+        For that, we first determine and clip off the common prefix of both paths.
+        Then, we sum up the costs for exiting from the source path to the common
+        ancestor and the costs for visiting the target path.
+
+        Parameters
+        ----------
+        source: List[int]
+            The source path, that is the address of a node.
+
+        target: List[int]
+            The target path, that is the address of a node.
+        
+        Returns
+        -------
+        float
+            The probability of walking along the path between the given `source` and `target`.
+        """
+        if len(source) > 0 and len(target) > 0 and source[0] == target[0]:
+            return self.code_book[source[0]].get_walk_probability(source[1:], target[1:])
+
+        return self.get_path_probability_reverse(source) \
+             * self.get_path_probability_forward(target, self.code_book[source[0]].enter)
+
+    def get_path_probability_forward(self, path: List[int], exclude: float) -> float:
+        """
+        Probability of descending along a path.
+
+        Parameters
+        ----------
+        path: List[int]
+            A path that addresses a node.
+        
+        Returns
+        -------
+        float
+            The probability of descending along the given path.
+        """
+        if len(path) == 1:
+            return self.code_book[path[0]].enter / (self.normaliser - exclude)
+
+        return self.code_book[path[0]].enter / (self.normaliser - exclude) \
+             * self.code_book[path[0]].get_path_probability_forward(path[1:], self.code_book[path[0]].exit)
+
+    def get_path_probability_reverse(self, path: List[int]) -> float:
+        """
+        The probability of ascending along a path without visiting the 
+        starting node.
+
+        Parameters
+        ----------
+        path: List[int]
+            A path that addresses a node.
+
+        Returns
+        -------
+        float
+            The probability of ascending along the given path.
+        """
+        if len(path) == 1:
+            return 1.0
+
+        return self.code_book[path[0]].exit / (self.code_book[path[0]].normaliser - self.code_book[path[0]].code_book[path[1]].enter) \
+             * self.code_book[path[0]].get_path_probability_reverse(path[1:])
